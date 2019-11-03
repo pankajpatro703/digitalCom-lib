accb9508b09973a1783f4ec6a8f6b80e2fcc928f
Pankajkumar Patro <45874442+pankajpatro703@users.noreply.github.com>
2019-11-03 20:25:46 +0530

    Update LZW.py

--- LZWcompression/LZW.py
+++ LZWcompression/LZW.py
@@ -4,6 +4,7 @@ def compress(uncompressed):
     for i in uncompressed:
         l.append(i)
     l = list(dict.fromkeys(l))
+    l.sort()
     for i in range(len(l)):
         dictionary[l[i]] = i + 1
     initial = l
@@ -19,7 +20,7 @@ def compress(uncompressed):
             dictionary[wc] = dict_size
             dict_size += 1
             w = c
-    if w:
+    if (w!=""):
         result.append(dictionary[w])
     return result, initial, dictionary
 
@@ -28,21 +29,39 @@ def uncompress(compressed,initial):
     data=""
     for i in range(len(initial)):
         dictionary[initial[i]] = i + 1
-    count=len(dictionary)
-    for i in compressed:
-        if(i in dictionary.values()):
-            data=data+list(dictionary.keys())[list(dictionary.values()).index(i)]
-        else:
-
-            
+    i,j,flag=0,0,0
+    while(i<len(compressed)):
+        if(compressed[i] in dictionary.values()):
+            if(flag!=0):
+                data=data[:len(data)-flag]
+                flag=0
+            data=data+list(dictionary.keys())[list(dictionary.values()).index(compressed[i])]
+            i=i+1
+        elif(compressed[i]>len(dictionary)):
+            newdata=data[j:]
+            w=""
+            dict_size=len(dictionary)
+            for c in newdata:        
+                wc=w+c
+                if(wc in dictionary):
+                    w=wc
+                    j+=1
+                else:
+                    dictionary[wc] = dict_size+1
+                    dict_size += 1
+                    w = c
+                    break
+                if(j==len(data)):
+                        data+=wc
+                        j-=len(wc)
+                        flag=len(wc)
     return data
 
-
-data = input("Enter a string: ")
-compressed, initial, dictionary = compress(data)
-print("Compressed data is: ")
-print(compressed)
-print(dictionary)
-print(initial)
-decoded=uncompress(compressed,initial)
-print(decoded)
+#data = input("Enter a string: ")
+#compressed, initial, dictionary = compress(data)
+#print("Compressed data is: ")
+#print(compressed)
+#print(dictionary)
+#print(initial)
+#decoded=uncompress(compressed,initial)
+#print(decoded)
