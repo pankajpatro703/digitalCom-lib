66d628ee404d6d78cefcb9d763642a29fcfadb92
Pankajkumar Patro <45874442+pankajpatro703@users.noreply.github.com>
2020-02-07 01:03:06 +0530

    Documented

--- PlayfairCipher/playfair.py
+++ PlayfairCipher/playfair.py
@@ -1,30 +1,46 @@
 #Encryption and Decryption using playfair cipher
+###########################
+# @Author: pankajpatro703 #
+#  Licensed under GPL v3  #
+###########################
 
-SIZE=9
+SIZE=9      #for a 9x9 sized playfair square
 
-#orglist = [chr(i) for i in range(32,127)]
+#orglist = [chr(i) for i in range(32,127)] #useful ASCII codes
 orglist = [chr(i) for i in range(32+33,32+59)] + \
         [chr(i) for i in range(32+65,32+91)] + \
         [chr(i) for i in range(32+16,32+26)] + \
         [chr(i) for i in range(32+0,32+16)] + \
         [chr(i) for i in range(32+91,32+95)]
 #A-Z,a-z,0-9,symbols
-# ~ may be required specially
+# '|' is required for padding and should not be used in the message
 
-def removeDuplicate(str, n):       
+def removeDuplicate(lst):
+    '''
+    This method removes duplicate characters in a list without 
+    losing the order in which the characters appaer in the original list.
+    :param lst: original list of characters
+    :return: string with repeated charcaters removed 
+    '''       
     index = 0
+    n = len(lst)
     for i in range(0, n): 
-        for j in range(0, i + 1): 
-            if (str[i] == str[j]): 
+        for j in range(0, i+1): 
+            if (lst[i] == lst[j]): 
                 break 
         if (j == i): 
-            str[index] = str[i] 
+            lst[index] = lst[i] 
             index += 1
-    return("".join(str[:index])) 
+    return("".join(lst[:index])) 
 
 def getSquare(secretkey):
+    '''
+    This method generates a SIZE*SIZE square list containing all characters that can be used in a message
+    :param secretkey: string type secret key to generate the Square.
+    :return square: square list of ASCII characters 
+    '''
     square = []
-    key = removeDuplicate(list(secretkey), len(secretkey))
+    key = removeDuplicate(list(secretkey))
     keylist = [char for char in key]
     for i in orglist:
         if(i not in keylist):
@@ -35,53 +51,100 @@ def getSquare(secretkey):
             row.append(keylist[0])
             keylist.pop(0)
         square.append(row)
-    return square
+    return(square)
+
+def getLocation(x, square):
+    '''
+    This method finds the location of an element on a square list.
+    :param x: element whose location is to be found 
+    :param square: square with list of elements
+    :return x_loc: (row, column) location of x in square
+    '''
+    x_loc = 0
+    for i in range(len(square)):
+        if x in square[i]:
+            j = square[i].index(x)
+            x_loc = (i, j)
+    return(x_loc)    
 
 def getcode(l, r, square, operation):
-    for i in range(len(square)):
-        if l in square[i]:
-            j = square[i].index(l)
-            l_loc = (i, j)
-        if r in square[i]:
-            j = square[i].index(r)
-            r_loc = (i, j)
-    if(l_loc[0]==r_loc[0] and l_loc[1]==r_loc[1]):
-        #dont change    
-    elif(l_loc[0]==r_loc[0]):
-        #same row
-    elif(l_loc[1]==r_loc[1]):
-        #same column
+    '''
+    This method obtains the encrypted form of character pair.
+    :param l: first character in the pair
+    :param r: second character in the pair
+    :param square: square list of character map
+    :param operation: defines the opertaion - 'encrypt' or 'decrypt'
+    :return code: encrypted character pair 
+    '''
+    l_loc = getLocation(l, square)
+    r_loc = getLocation(r, square)
+    if(l_loc[0] != r_loc[0] and l_loc[1] != r_loc[1]):
+        #check block, same row different column
+        code = square[l_loc[0]][r_loc[1]] + square[r_loc[0]][l_loc[1]]
+    elif(l_loc[0] == r_loc[0] and l_loc[1] == r_loc[1]):
+        #repeated character, no change
+        code = square[l_loc[0]][l_loc[1]] + square[r_loc[0]][r_loc[1]]
     else:
-        #check block
-
+        n = 0
+        if(operation == 'encrypt'):
+            n = 1
+        elif(operation == 'decrypt'):
+            n = -1
+        if(l_loc[1]==r_loc[1]):
+            #same column
+            code = square[(l_loc[0]+n) % SIZE][l_loc[1]] + square[(r_loc[0]+n) % SIZE][r_loc[1]]
+        elif(l_loc[0]==r_loc[0]):
+            #same row
+            code = square[l_loc[0]][(l_loc[1]+n) % SIZE] + square[r_loc[0]][(r_loc[1]+n) % SIZE]
     return code
 
 
 def playfairEncrypt(plaintext, square):
+    '''
+    This method encrypts the plaintext using playfair cipher.
+    :param plaintext: original string
+    :param square: square list of characters obtained from key
+    :return ciphertext: cipher text after encryption
+    '''
+    if(len(plaintext)%2==1):
+        plaintext = plaintext[:] + '|'
     pairs = []
     newpairs = []
-    for i in range(len(plaintext), 2):
-        pairs.append(plaintext[i:i+2])
+    for i in range(0, len(plaintext), 2):
+        pairs.append(plaintext[i: i+2])
     for i in pairs:
         newpairs.append(getcode(i[0], i[1], square, 'encrypt'))
     ciphertext = "".join(newpairs)
     return ciphertext
 
 def playfairDecrypt(ciphertext, square):
+    '''
+    This method decrypts the ciphertext using playfair cipher.
+    :param ciphertext: original string
+    :param square: square list of characters obtained from key
+    :return deciphertext: deciphered text after decryption
+    '''
     pairs = []
     newpairs = []
-    for i in range(len(ciphertext), 2):
-        pairs.append(ciphertext[i:i+2])
+    for i in range(0, len(ciphertext), 2):
+        pairs.append(ciphertext[i: i+2])
     for i in pairs:
         newpairs.append(getcode(i[0], i[1], square, 'decrypt'))
     deciphertext = "".join(newpairs)
+    if(deciphertext[-1] == '|'):
+        deciphertext = deciphertext[: -1]
     return deciphertext
 
 if (__name__ == '__main__'):
-    secretkey = 'ThisIsASecretKey'
-    msg = "ThisIsTheMessageToBeProtected"
+    secretkey = 'This Is A Secret Key That Is Required To Encrypt & Decrypt'
+    msg = 'This is the message to be protected'
     new_square = getSquare(secretkey)
+    print('Square map:')
+    for row in new_square:
+        print(row)
     enc = playfairEncrypt(msg, new_square)
+    print('Encrypted message:')
     print(enc)
     dec = playfairDecrypt(enc, new_square)
+    print('Decrypted message:')
     print(dec)
