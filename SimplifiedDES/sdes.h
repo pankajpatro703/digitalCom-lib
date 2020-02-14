8ee238facff3262fc131bd26bfd4552810304b60
Pankajkumar Patro <45874442+pankajpatro703@users.noreply.github.com>
2020-02-14 21:29:22 +0530

    Added License Notice

--- SimplifiedDES/sdes.h
+++ SimplifiedDES/sdes.h
@@ -1,3 +1,26 @@
+/*
+    This file is part of DigiCom-algorithms.
+    
+    DigiCom-algorithms is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    DigiCom-algorithms is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+    
+    You should have received a copy of the GNU Lesser General Public License
+    along with DigiCom-algorithms.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+//Simplified Data Encryption Standard algorithm for encryption and decryption
+//////////////////////////////////
+//   @Author: pankajpatro703    //
+// Licensed under Lesser GPL v3 //
+//////////////////////////////////
+
 #include<stdio.h>
 #include<string.h>
 int i,cnt=0;
