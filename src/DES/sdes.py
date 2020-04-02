5e76884d35a19d970727b59d8fc21f224db5349c
“pankajpatro703” <pankajkumar.p@somaiya.edu>
2020-04-02 23:16:01 +0530

    modified global var names

--- src/DES/sdes.py
+++ src/DES/sdes.py
@@ -24,12 +24,12 @@
 #Sequences defined by SDES algorithm
 p10 = [3,5,2,7,4,10,1,9,8,6]
 p8 = [6,3,7,4,8,5,10,9]
-IP = [2,6,3,1,4,8,5,7]
+iP = [2,6,3,1,4,8,5,7]
 invIP = [4,1,3,5,7,2,8,6]
 
 s0 = [[1,0,3,2],[3,2,1,0],[0,2,1,3],[3,1,3,2]]
 s1 = [[0,1,2,3],[2,0,1,3],[3,0,1,0],[2,1,0,3]]
-EP = [4,1,2,3,2,3,4,1]
+eP = [4,1,2,3,2,3,4,1]
 p4 = [2,4,3,1]
 
 def permutation(seq, data, datasize):
@@ -63,7 +63,7 @@ def fk(base, k):
     :param k: 8-bit generated key 
     :return: 8-bit data obtained after applying key function
     '''
-    pos = format(permutation(EP, base & 0b1111, 4) ^ k, '08b')
+    pos = format(permutation(eP, base & 0b1111, 4) ^ k, '08b')
     value = format(s0[int(pos[0]+pos[3], 2)][int(pos[1]+pos[2], 2)], '02b') + \
         format(s1[int(pos[4]+pos[7], 2)][int(pos[5]+pos[6], 2)], '02b')
     lpt = permutation(p4, int(value, 2), 4) ^ ((base>>4) & 0b1111)
@@ -101,7 +101,7 @@ def sdesEncrypt(plaintext, key):
     :return ciphertext: 8-bit cipher text after encryption 
     '''
     (k1, k2) = keygen(key)
-    ciphertext = permutation(invIP, fk(sw(fk(permutation(IP, plaintext, 8), k1), 4), k2), 8)
+    ciphertext = permutation(invIP, fk(sw(fk(permutation(iP, plaintext, 8), k1), 4), k2), 8)
     return(ciphertext)
 
 def sdesDecrypt(ciphertext, key):
@@ -112,7 +112,7 @@ def sdesDecrypt(ciphertext, key):
     :return decipheredtext: 8-bit deciphered text after decryption
     '''
     (k1, k2) = keygen(key)
-    decipheredtext = permutation(invIP, fk(sw(fk(permutation(IP, ciphertext, 8), k2), 4), k1), 8)
+    decipheredtext = permutation(invIP, fk(sw(fk(permutation(iP, ciphertext, 8), k2), 4), k1), 8)
     return decipheredtext
 
 if(__name__=='__main__'):
